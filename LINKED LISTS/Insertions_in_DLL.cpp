f9dade4efe16277f1db3a463f76aadc17b373fe4
OgierCodes <schwabeb444@gmail.com>
2026-07-28 13:56:09 +0530

    Update Insertions_in_DLL.cpp

--- LINKED LISTS/Insertions_in_DLL.cpp
+++ LINKED LISTS/Insertions_in_DLL.cpp
@@ -107,7 +107,36 @@ int main() {
 }
 
 //Add a node at the end in a DLL
+void insertion(Node** head_ref, int new_data){
+    Node* new_node = new Node(new_data);
+    Node* last = *head_ref;
+    new_node->next = NULL;
+    if(*head_ref == NULL){
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
+    while(last->next != NULL){
+        last = last->next;
+    }
+    last->next = new_node;
+    new_node->prev = last;
+    return;
+}
 
+int main(){
+    Node* head = NULL;
+    insertion(&head, 10);
+    insertion(&head, 20);
+    insertion(&head, 30);
+
+    insertion(&head, 50); //new_node
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
 
 
 
